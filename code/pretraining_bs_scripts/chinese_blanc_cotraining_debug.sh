9f37c218e04ca0cecf7a199c83f6a6257d1bfd3d
mindahu <noble6emc2@gmail.com>
2021-01-11 18:03:15 +0800

    model debugging

---
+++ code/pretraining_bs_scripts/chinese_blanc_cotraining_debug.sh
@@ -0,0 +1,27 @@
+PYTHONIOENCODING=utf-8 CUDA_VISIBLE_DEVICES=0,1,2 python3 src/lm/BLANC/run_mrqa_blanc_pretraining_chinese.py \
+  --do_train \
+  --model /apdcephfs/common/mindahu/bert-base-chinese.tar.gz \
+  --tokenizer /apdcephfs/common/mindahu/bert-base-chinese-vocab.txt \
+  --train_file $DATA_DIR/all.json \
+  --dev_file $DATA_DIR/dev.jsonl.gz \
+  --test_file $DATA_DIR/test.jsonl.gz \
+  --train_batch_size 8 \
+  --eval_batch_size 16  \
+  --learning_rate 2e-5 \
+  --num_train_epochs 1 \
+  --max_seq_length 384 \
+  --doc_stride 128 \
+  --eval_metric span_f1 \
+  --output_dir ./checkpoints/pertraining_cn_cotraining/$LABEL/$SEED \
+  --eval_per_epoch 20 \
+  --seed $SEED \
+  --geometric_p $GEOP \
+  --window_size $WINS \
+  --lmb $LMB \
+  --is_co_training true \
+  --co_training_mode moving_loss\
+  --enqueue_thread_num 2 \
+  --num_iteration 37500 \
+  --moving_loss_num 32 \
+  --theta 0.8 \
+  --debug true
--- code/src/lm/BLANC/pytorch_pretrained_bert/blanc.py
+++ code/src/lm/BLANC/pytorch_pretrained_bert/blanc.py
@@ -1267,6 +1267,7 @@ class BLANC(BertPreTrainedModel):
             dist_total_loss = - 2.0 * dist_total_loss
             
             if lmbs is None:
+                print("No Lmbs !!!!!!!!!!")
                 loss_fct = CrossEntropyLoss(ignore_index=ignored_index)
                 start_loss = loss_fct(start_logits, start_positions)
                 end_loss = loss_fct(end_logits, end_positions)
@@ -1275,6 +1276,7 @@ class BLANC(BertPreTrainedModel):
                 total_loss = (1.0 - lmb) * f_loss + lmb * dist_total_loss
                 return (total_loss, dist_total_loss, reduced_total_dist_losses)
             else:
+                print("Lmbs:", lmbs)
                 loss_fct = CrossEntropyLoss(ignore_index=ignored_index, reduction='none')
                 start_losses = loss_fct(start_logits, start_positions)
                 end_losses = loss_fct(end_logits, end_positions)
