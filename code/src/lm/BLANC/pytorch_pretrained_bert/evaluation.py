a7ebd58d9ebb78f0e3a859507711ae1691df1ede
Hu Jerry <noble6emc2@gmail.com>
2021-04-13 21:54:37 +0800

    evaluator debugged

--- code/src/lm/BLANC/pytorch_pretrained_bert/evaluation.py
+++ code/src/lm/BLANC/pytorch_pretrained_bert/evaluation.py
@@ -851,8 +851,8 @@ class SQuADEvaluator:
 
     @staticmethod
     def find_all_best_thresh(main_eval, preds, exact_raw, f1_raw, na_probs, qid_to_has_ans):
-        best_exact, exact_thresh = find_best_thresh(preds, exact_raw, na_probs, qid_to_has_ans)
-        best_f1, f1_thresh = find_best_thresh(preds, f1_raw, na_probs, qid_to_has_ans)
+        best_exact, exact_thresh = SQuADEvaluator.find_best_thresh(preds, exact_raw, na_probs, qid_to_has_ans)
+        best_f1, f1_thresh = SQuADEvaluator.find_best_thresh(preds, f1_raw, na_probs, qid_to_has_ans)
         main_eval['best_exact'] = best_exact
         main_eval['best_exact_thresh'] = exact_thresh
         main_eval['best_f1'] = best_f1
@@ -881,7 +881,7 @@ class SQuADEvaluator:
                                             end_logits=end_logits))
 
         preds, nbest_preds = \
-            make_predictions(eval_examples, eval_features, all_results,
+            SQuADEvaluator.make_predictions(eval_examples, eval_features, all_results,
                             args.n_best_size, args.max_answer_length,
                             args.do_lower_case, args.verbose_logging)
         
